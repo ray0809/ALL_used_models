28f268a99272603e5ae53aa28e3aef4d300ef13c
ray <ray@raydeMacBook-Pro.local>
2019-01-30 21:59:19 +0800

    Standard metric learning process

---
+++ triplet/model.py
@@ -0,0 +1,55 @@
+#coding=utf-8
+import keras.backend as K
+from keras.models import Model
+from keras.layers import Input,Flatten,Dropout,Dense,Lambda,Dot
+from keras.applications import resnet50
+
+def Normalize(x):
+    x = K.l2_normalize(x, axis=-1)
+    return x
+
+def Distance(x, margin=1):
+    q, p, n = x[0], x[1], x[2]
+    dis_qp = K.sqrt(K.sum(K.square(q - p), axis=-1, keepdims=True))
+    dis_qn = K.sqrt(K.sum(K.square(q - n), axis=-1, keepdims=True))
+    loss = K.relu(dis_qp - dis_qn + margin)
+    return loss
+
+
+def crtModel(freezen=False):
+    resnet = resnet50.ResNet50(include_top=False,pooling='avg')
+    if freezen:
+        for layer in resnet.layers:
+            layer.trainable = False
+    inputs = Input(shape=(None,None,3))
+    x = Lambda(resnet50.preprocess_input,arguments={'mode':'tf'})(inputs)
+    x = resnet(x)
+    x = Dense(512,name='embedding')(x)
+    embedding = Lambda(Normalize, name='l2_embedding')(x)
+    model = Model(inputs, embedding)
+    model.summary()
+    return model
+
+
+def TripletNet(margin=1, freezen=False):
+    base_model = crtModel(freezen=freezen)
+    base_model.load_weights('weights/20190130_full_8583.h5',by_name=True)
+    input_q = Input(shape=(None, None, 3))
+    input_p = Input(shape=(None, None, 3))
+    input_n = Input(shape=(None, None, 3))
+
+    encode_q = base_model(input_q)
+    encode_p = base_model(input_p)
+    encode_n = base_model(input_n)
+
+    # dis_q_p = Dot(axes=-1, normalize=False)([encode_q,encode_p])
+    # dis_q_n = Dot(axes=-1, normalize=False)([encode_q,encode_n])
+
+    # cos distance
+    # loss = Lambda(lambda x: K.relu(margin+x[1]-x[0]))([dis_q_p,dis_q_n])
+    # absolute distance
+    loss = Lambda(Distance,arguments={'margin':margin})([encode_q,encode_p,encode_n])
+
+    model_train = Model([input_q,input_p,input_n],loss)
+    model_test = Model(input_q,encode_q)
+    return model_train, model_test
